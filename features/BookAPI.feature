86ae7d7110302700b858575ed362c21e35fcb80c
alpayalyn <alpay.alin@useinsider.com>
2022-01-14 18:33:56 +0300

    Features added, Scenarios added, Context argument used, environment file used for after_scenario

--- features/BookAPI.feature
+++ features/BookAPI.feature
@@ -1,6 +1,8 @@
 # Created by alpay at 14.01.2022
-Feature: # Enter feature name here
+Feature: Verify if Books are added and deleted using Library API
   # Enter feature description here
 
-  Scenario: # Enter scenario name here
-    # Enter steps here
+  Scenario: Verify AddBook API functionality
+    Given the Book details which needs to be added to Library
+    When we execute the AddBook PostAPI method
+    Then book is successfully added
---
+++ features/steps/stepImpl.py
@@ -0,0 +1,26 @@
+import requests
+from behave import *
+from payLoad import *
+from utilities.resources import *
+from utilities.configurations import *
+import json
+
+@given('the Book details which needs to be added to Library') # to be able to use the local variables in other modules below, you can you context. to use the VARIABLE.
+def step_impl(context):
+    context.url = getConfig()['API']['endpoint'] + ApiResources.addBook
+    context.headers = {"Content-Type": "application/json"}
+    context.payLoad = addBookPayload("asdasd")
+
+@when('we execute the AddBook PostAPI method'):
+def step_impl(context):
+    context.addBook_response = requests.post(context.url, json=context.addBook_response, headers=context.headers, ) # You sent POST and wait for repsonse.
+
+@then('book is successfully added'): # You want to retrieve the Book ID
+def step_impl(context):
+    print(context.addBook_response.json())
+    response_json = context.addBook_response.json()
+    print(type(response_json))
+
+    context.bookId = response_json['ID']
+    print(bookId)
+    assert response_json["Msg"] == "successfully added"
