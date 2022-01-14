a751420113d89dfce2eaac2f113333c08892fe9f
alpayalyn <alpay.alin@useinsider.com>
2022-01-14 21:12:38 +0300

    Addition to upper side of features, and adding some ifs into hook which is after_Scenario to prevent wrong features to be affect from the hook.

--- features/steps/environment.py
+++ features/steps/environment.py
@@ -1,13 +1,14 @@
 import requests
 
 def after_scenario(context, scenario):
-    response_deleteBook = requests.post('http://216.10.245.166/Library/DeleteBook.php', json={
+    if "library" in scenario.tags:
+        response_deleteBook = requests.post('http://216.10.245.166/Library/DeleteBook.php', json={
 
-        "ID": context.bookId
-    }, headers={"Content-Type": "application/json"},
-                                        )
-    assert response_deleteBook.status_code == 200
-    context.res_json = response_deleteBook.json()
+            "ID": context.bookId
+        }, headers={"Content-Type": "application/json"},
+                                            )
+        assert response_deleteBook.status_code == 200
+        context.res_json = response_deleteBook.json()
 
-    print(res_json["msg"])
-    assert res_json["msg"] == "book is successfully deleted."
+        print(res_json["msg"])
+        assert res_json["msg"] == "book is successfully deleted."
